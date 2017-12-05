6946acb5051a1218a9f8dd63e175f080777e2c3e
MohieddineCHATTI <mohieddinechatti@gmail.com>
2017-12-05 21:02:22 +0100

    vetoquiz time score and logic added for the first time

--- vetoQuiz.js
+++ vetoQuiz.js
@@ -1,4 +1,4 @@
-var all_qst=[
+var easy_qst=[
     {
         qst: "what s the capital city of Algeria ?",
         opt1: "algeirs",
@@ -30,15 +30,274 @@ var all_qst=[
         opt3: "us",
         opt4: "germany",
         correct_answers: ["algeria"]
+    },
+    {
+        qst: "numbers that are lowwer than 10 ?",
+        opt1: "5",
+        opt2: "7",
+        opt3: "13",
+        opt4: "100",
+        correct_answers: ["5", "7"]
+    },
+    {
+        qst: "the day after saturday ?",
+        opt1: "friday",
+        opt2: "wednesday",
+        opt3: "monday",
+        opt4: "none of the above",
+        correct_answers: ["none of the above"]
+    },
+    {
+        qst: "what s the best country in the world ?",
+        opt1: "algeria",
+        opt2: "togo",
+        opt3: "us",
+        opt4: "germany",
+        correct_answers: ["algeria"]
+    },
+    {
+        qst: "numbers that are lowwer than 10 ?",
+        opt1: "5",
+        opt2: "7",
+        opt3: "13",
+        opt4: "100",
+        correct_answers: ["5", "7"]
+    },
+    {
+        qst: "the day after saturday ?",
+        opt1: "friday",
+        opt2: "wednesday",
+        opt3: "monday",
+        opt4: "none of the above",
+        correct_answers: ["none of the above"]
+    },
+    {
+        qst: "what s the best country in the world ?",
+        opt1: "algeria",
+        opt2: "togo",
+        opt3: "us",
+        opt4: "germany",
+        correct_answers: ["algeria"]
     }
     
-    
-    
-    
-    
-    
+    ];
+
+var medium_qst=[
+    {
+        qst: "what s the capital city of Algeria ?",
+        opt1: "algeirs",
+        opt2: "biskra",
+        opt3: "feliache",
+        opt4: "cabinet vet dhal3a",
+        correct_answers: ["algeirs"]
+    },
+    {
+        qst: "numbers that are lowwer than 10 ?",
+        opt1: "5",
+        opt2: "7",
+        opt3: "13",
+        opt4: "100",
+        correct_answers: ["5", "7"]
+    },
+    {
+        qst: "the day after saturday ?",
+        opt1: "friday",
+        opt2: "wednesday",
+        opt3: "monday",
+        opt4: "none of the above",
+        correct_answers: ["none of the above"]
+    },
+    {
+        qst: "what s the best country in the world ?",
+        opt1: "algeria",
+        opt2: "togo",
+        opt3: "us",
+        opt4: "germany",
+        correct_answers: ["algeria"]
+    },
+    {
+        qst: "numbers that are lowwer than 10 ?",
+        opt1: "5",
+        opt2: "7",
+        opt3: "13",
+        opt4: "100",
+        correct_answers: ["5", "7"]
+    },
+    {
+        qst: "the day after saturday ?",
+        opt1: "friday",
+        opt2: "wednesday",
+        opt3: "monday",
+        opt4: "none of the above",
+        correct_answers: ["none of the above"]
+    },
+    {
+        qst: "what s the best country in the world ?",
+        opt1: "algeria",
+        opt2: "togo",
+        opt3: "us",
+        opt4: "germany",
+        correct_answers: ["algeria"]
+    },
+    {
+        qst: "numbers that are lowwer than 10 ?",
+        opt1: "5",
+        opt2: "7",
+        opt3: "13",
+        opt4: "100",
+        correct_answers: ["5", "7"]
+    },
+    {
+        qst: "the day after saturday ?",
+        opt1: "friday",
+        opt2: "wednesday",
+        opt3: "monday",
+        opt4: "none of the above",
+        correct_answers: ["none of the above"]
+    },
+    {
+        qst: "what s the best country in the world ?",
+        opt1: "algeria",
+        opt2: "togo",
+        opt3: "us",
+        opt4: "germany",
+        correct_answers: ["algeria"]
+    }
 ];
+
+
+var hard_qst=[
+    {
+        qst: "what s the capital city of Algeria ?",
+        opt1: "algeirs",
+        opt2: "biskra",
+        opt3: "feliache",
+        opt4: "cabinet vet dhal3a",
+        correct_answers: ["algeirs"]
+    },
+    {
+        qst: "numbers that are lowwer than 10 ?",
+        opt1: "5",
+        opt2: "7",
+        opt3: "13",
+        opt4: "100",
+        correct_answers: ["5", "7"]
+    },
+    {
+        qst: "the day after saturday ?",
+        opt1: "friday",
+        opt2: "wednesday",
+        opt3: "monday",
+        opt4: "none of the above",
+        correct_answers: ["none of the above"]
+    },
+    {
+        qst: "what s the best country in the world ?",
+        opt1: "algeria",
+        opt2: "togo",
+        opt3: "us",
+        opt4: "germany",
+        correct_answers: ["algeria"]
+    },
+    {
+        qst: "numbers that are lowwer than 10 ?",
+        opt1: "5",
+        opt2: "7",
+        opt3: "13",
+        opt4: "100",
+        correct_answers: ["5", "7"]
+    },
+    {
+        qst: "the day after saturday ?",
+        opt1: "friday",
+        opt2: "wednesday",
+        opt3: "monday",
+        opt4: "none of the above",
+        correct_answers: ["none of the above"]
+    },
+    {
+        qst: "what s the best country in the world ?",
+        opt1: "algeria",
+        opt2: "togo",
+        opt3: "us",
+        opt4: "germany",
+        correct_answers: ["algeria"]
+    },
+    {
+        qst: "numbers that are lowwer than 10 ?",
+        opt1: "5",
+        opt2: "7",
+        opt3: "13",
+        opt4: "100",
+        correct_answers: ["5", "7"]
+    },
+    {
+        qst: "the day after saturday ?",
+        opt1: "friday",
+        opt2: "wednesday",
+        opt3: "monday",
+        opt4: "none of the above",
+        correct_answers: ["none of the above"]
+    },
+    {
+        qst: "what s the best country in the world ?",
+        opt1: "algeria",
+        opt2: "togo",
+        opt3: "us",
+        opt4: "germany",
+        correct_answers: ["algeria"]
+    }
+];
+
+var all_qst=[];
+var easy_index=[];
+var medium_index=[];
+var hard_index=[];
+var index=0;
+var j;
+
+
+do {
     
+    
+    if (index<3){
+        
+     j = Math.floor(Math.random()*easy_qst.length);
+        
+    if (!easy_index.includes(j)){
+        all_qst.push(easy_qst[j]);
+        easy_index.push(j);
+        index++;
+    }
+    }
+
+    if (index>2 && index<9){
+        
+     j = Math.floor(Math.random()*medium_qst.length);
+        
+    if (!medium_index.includes(j)){
+        all_qst.push(medium_qst[j]);
+        medium_index.push(j);
+        index++;
+    }
+    }
+
+    if (index>8){
+        
+     j = Math.floor(Math.random()*hard_qst.length);
+        
+    if (!hard_index.includes(j)){
+        all_qst.push(hard_qst[j]);
+        hard_index.push(j);
+        index++;
+    }
+    }
+
+    
+    } while (index<11);
+
+
+console.log(all_qst);
     
 var i=0;
 var checked=[];
@@ -47,9 +306,11 @@ var t;
 var score = 0;
 var correct_ans=0;
 var wrong_ans=0;
+var times_up ;
 // next question function
 function next_qst(){
 checked=[];
+times_up = false;
 if (i<all_qst.length){
 console.log('app is workin working');
 document.getElementById("qst").innerHTML=(all_qst[i].qst);
@@ -68,6 +329,10 @@ console.log("t is "+t);
 } else {
     console.log("time is up");
     clearInterval(count);
+    times_up = true;
+    setTimeout(function(){
+    ans_check();
+    },3000);
 }
 
 },1000) ; 
@@ -86,14 +351,22 @@ if (!checked.includes($(this).text())){
 
 
 function ans_check(){
-if (!checked.length<1){
+if (times_up){
+    checking = false;
+    console.log(checked.sort());
+    console.log(all_qst[i-1].correct_answers.sort());
+    console.log(checking);
+    wrong_ans++;
+}
+
+else if (!checked.length<1){
 clearInterval(count);
 console.log(t);
 console.log(checked.sort());
 console.log(all_qst[i-1].correct_answers.sort());
     if (checked.length==all_qst[i-1].correct_answers.length){
     var k=0;
-    var checking = true;
+    checking = true;
     do{
         if (checked.sort()[k]!=all_qst[i-1].correct_answers.sort()[k])
         checking= false;
@@ -110,13 +383,13 @@ console.log(all_qst[i-1].correct_answers.sort());
         } else { 
         wrong_ans++;
         }
-        console.log(score);
-        console.log(correct_ans);
-        console.log(wrong_ans);
         
     }else {
         alert("please answer the question first");
     }
+        console.log("your score is "+score);
+        console.log("correct answers "+correct_ans);
+        console.log("wrong answers "+wrong_ans);
 }
 
 
